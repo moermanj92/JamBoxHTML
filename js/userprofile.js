df61627fbb20972c8948cffb674843b833652d69
Jeroen Moerman <moermanj@visocloud.org>
2016-03-30 17:30:26 +0200

    Visual update
    Visual update for user page

--- js/userprofile.js
+++ js/userprofile.js
@@ -1,4 +1,4 @@
-function filter_content(toshow, tohide1, tohide2){
+/*function filter_content(toshow, tohide1, tohide2){
 	console.log("test");
 	var classToShow = "." + toshow + "-content"; //create class selectors
 	var classToHide1 = "." + tohide1 + "-content";
@@ -8,17 +8,26 @@ function filter_content(toshow, tohide1, tohide2){
 	$(classToHide1).hide();
 	$(classToHide2).hide();
 };
-
+*/
 $(".contactme").click(function(){
-	filter_content("contactme", "mybands", "myskills");
+	//filter_content("contactme", "mybands", "myskills");
+	$(".contactme").addClass("buttonActiveContact");
+	$(".myskills").removeClass("buttonActiveSkills");
+	$(".mybands").removeClass("buttonActiveBands");
 });
 $(".myskills").click(function(){
-	filter_content("myskills", "mybands", "contactme");
+	//filter_content("myskills", "mybands", "contactme");
+	$(".myskills").addClass("buttonActiveSkills");
+	$(".contactme").removeClass("buttonActiveContact");
+	$(".mybands").removeClass("buttonActiveBands");
 });
 $(".mybands").click(function(){
-	filter_content("mybands", "contactme", "myskills");
+	//filter_content("mybands", "contactme", "myskills");
+	$(".mybands").addClass("buttonActiveBands");
+	$(".myskills").removeClass("buttonActiveSkills");
+	$(".contactme").removeClass("buttonActiveContact");
 });
-
+/*
 $(".contactme-content").hide();
 $(".mybands-content").hide();
-$(".myskills-content").hide();
+$(".myskills-content").hide();*/
