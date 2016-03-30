2c8ac4269f12d587e2e27a650f97b970ab298e49
Jeroen Moerman <moermanj@visocloud.org>
2016-03-30 14:59:01 +0200

    User profile added
    Basic user profile GUI
    Added links from and to newsfeed

---
+++ js/userprofile.js
@@ -0,0 +1,26 @@
+function filter_content(toshow, tohide1, tohide2){
+	console.log("test");
+	var classToShow = "." + toshow + "-content"; //create class selectors
+	var classToHide1 = "." + tohide1 + "-content";
+	var classToHide2 = "." + tohide2 + "-content";
+
+	console.log(classToShow);
+
+	$(classToShow).show(); //show or hide created class selectors
+	$(classToHide1).hide();
+	$(classToHide2).hide();
+};
+
+$(".contactme").click(function(){
+	filter_content("contactme", "mybands", "myskills");
+});
+$(".myskills").click(function(){
+	filter_content("myskills", "mybands", "contactme");
+});
+$(".mybands").click(function(){
+	filter_content("mybands", "contactme", "myskills");
+});
+
+$(".contactme-content").hide();
+$(".mybands-content").hide();
+$(".myskills-content").hide();
