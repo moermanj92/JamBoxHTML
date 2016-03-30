8404bd0f15c2c2fbb4b2f57eb34a65152451ad3b
Jeroen Moerman <moermanj@visocloud.org>
2016-03-30 21:50:23 +0200

    Multiple pages added
    User page update
    Band page added
    Bands near you added
    Not fancy yet

---
+++ js/bandprofile.js
@@ -0,0 +1,37 @@
+/*function filter_content(toshow, tohide1, tohide2){
+	console.log("test");
+	var classToShow = "." + toshow + "-content"; //create class selectors
+	var classToHide1 = "." + tohide1 + "-content";
+	var classToHide2 = "." + tohide2 + "-content";
+
+	$(classToShow).show(); //show or hide created class selectors
+	$(classToHide1).hide();
+	$(classToHide2).hide();
+};
+*/
+$(".contactme").click(function(){
+	//filter_content("contactme", "mybands", "myskills");
+	$(".contactme").addClass("buttonActiveContact");
+	$(".myskills").removeClass("buttonActiveSkills");
+	$(".mybands").removeClass("buttonActiveBands");
+});
+$(".myskills").click(function(){
+	//filter_content("myskills", "mybands", "contactme");
+	$(".myskills").addClass("buttonActiveSkills");
+	$(".contactme").removeClass("buttonActiveContact");
+	$(".mybands").removeClass("buttonActiveBands");
+});
+$(".mybands").click(function(){
+	//filter_content("mybands", "contactme", "myskills");
+	$(".mybands").addClass("buttonActiveBands");
+	$(".myskills").removeClass("buttonActiveSkills");
+	$(".contactme").removeClass("buttonActiveContact");
+});
+
+/*$(window).on("hashchange", function () {
+    window.scrollTo(window.scrollX, window.scrollY - 100);
+});
+/*
+$(".contactme-content").hide();
+$(".mybands-content").hide();
+$(".myskills-content").hide();*/
